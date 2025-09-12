22e8d35f3c93c5b377244f8e57c27f42af418b71
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2025-09-12 15:40:55 +0000

    add action running script

---
+++ action-runner-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+
+
+helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
+helm repo update
+helm install actions-runner-controller actions-runner-controller/actions-runner-controller --namespace actions-runner-system --create-namespace
+
+INSTALLATION_NAME="arc-runner-set"
+NAMESPACE="arc-runners"
+GITHUB_CONFIG_URL="https://github.com/<your_enterprise/org/repo>"
+GITHUB_PAT="$GITHUB_TOKEN"
+
+helm install "${INSTALLATION_NAME}" \
+    --namespace "${NAMESPACE}" \
+    --create-namespace \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
