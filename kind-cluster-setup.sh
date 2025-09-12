8bb703e1906fdcf7e276952cec89cd3e36da01d0
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2025-09-12 15:03:31 +0000

    name cluster

--- kind-cluster-setup.sh
+++ kind-cluster-setup.sh
@@ -33,7 +33,7 @@ fi
 # https://github.com/kubernetes-sigs/kind/issues/2875
 # https://github.com/containerd/containerd/blob/main/docs/cri/config.md#registry-configuration
 # See: https://github.com/containerd/containerd/blob/main/docs/hosts.md
-cat <<EOF | kind create cluster --name $CLUSTER_NAME --config=-
+cat <<EOF | kind create cluster --name "$CLUSTER_NAME" --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
